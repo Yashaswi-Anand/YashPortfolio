441e6e9177db879bf29a3e5a30042714c36c8984
Yashaswi Anand <yash.kwr17@gmail.com>
2023-05-09 21:31:22 +0530

    modified

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Project() {
   return (
-    <div style={{backgroundImage:'radial-gradient(#FFFBF5, #C3ACD0)', height:'100vh'}}>Project</div>
+    <div style={{backgroundImage:'radial-gradient(#FFFBF5, #C3ACD0)', height:'100vh'}}>Projects</div>
   )
 }
 
