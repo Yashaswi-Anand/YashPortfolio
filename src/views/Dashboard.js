27f2e29cabdbcaae29171d28abed81bee8669588
Yashaswi Anand <yashaswi@upjai.ai>
2025-02-04 21:10:28 +0530

    background color added

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -6,37 +6,40 @@ import Experience from './Experience';
 import Education from './Education';
 import Project from './Project';
 import Contact from './Contact';
+import Pattern from '../components/Pattern';
 
 function Dashboard() {
   return (
-    <div>
-      <NavBar />
-      <div className="content">
-        <section id="home">
-          <Home/>
-        </section>
-        <section id="experience">
-          <Experience/>
-        </section>
-        <section id="skills">
-          <Skills/>
-        </section>
-        <section id="education">
-          <Education/>
-        </section>
-        <section id="project">
-          <Project/>
-        </section>
-        <section id="achievements">
-          <h2>Achievements</h2>
-          <p>Here are some of my significant achievements.</p>
-        </section>
-        <section id="contact">
-          <h2>Contact</h2>
-          <Contact/>
-        </section>
+    <Pattern component={
+      <div>
+        <NavBar />
+        <div className="content">
+          <section id="home">
+            <Home />
+          </section>
+          <section id="experience">
+            <Experience />
+          </section>
+          <section id="skills">
+            <Skills />
+          </section>
+          <section id="education">
+            <Education />
+          </section>
+          <section id="project">
+            <Project />
+          </section>
+          <section id="achievements">
+            <h2>Achievements</h2>
+            <p>Here are some of my significant achievements.</p>
+          </section>
+          <section id="contact">
+            <h2>Contact</h2>
+            <Contact />
+          </section>
+        </div>
       </div>
-    </div>
+    } />
   );
 }
 
