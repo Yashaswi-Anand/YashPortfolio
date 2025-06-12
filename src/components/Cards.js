fbe31a5574111f51569eadbc45d8922f8746bb06
Yashaswi Anand <yashaswi@upjao.ai>
2025-06-12 23:39:23 +0530

    modify

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -12,6 +12,7 @@ const Cards = ({ data }) => {
                                     src={data.image}
                                     alt="Card"
                                     width={'100%'} height={305}
+                                    style={{objectFit: 'fill'}}
                                 />
                             </div>
                         </div>
@@ -28,18 +29,15 @@ const Cards = ({ data }) => {
                         <div className="front-content">
                             <div>
                                 <small className="badge">{data.project_name}</small>
-                                <div className='m-t-10'>Content</div>
+                                <div className='m-t-10 content-text'>{data.content}</div>
                             </div>
                             <div className="description">
                                 <div className="title">
-                                    {/* <p className="title">
-                                        <strong>{data.project_name}</strong>
-                                    </p> */}
-                                    <svg fillRule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style={{ mixBlendMode: 'normal' }} textAnchor="none" fontSize="none" fontWeight="none" fontFamily="none" strokeDashoffset={0} strokeDasharray strokeMiterlimit={10} strokeLinejoin="miter" strokeLinecap="butt" strokeWidth={1} stroke="none" fillRule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z" /></g></g></svg>
+                                    <svg className='badge' fillRule="nonzero" height="15px" width="15px" viewBox="0,0,256,256" xmlnsXlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g style={{ mixBlendMode: 'normal' }} textAnchor="none" fontSize="none" fontWeight="none" fontFamily="none" strokeDashoffset={0} strokeDasharray strokeMiterlimit={10} strokeLinejoin="miter" strokeLinecap="butt" strokeWidth={1} stroke="none" fillRule="nonzero" fill="#20c997"><g transform="scale(8,8)"><path d="M25,27l-9,-6.75l-9,6.75v-23h18z" /></g></g></svg>
+                                    <span className='badge project-link'>Github</span>
+                                    <span className='badge project-link'>Live</span>
                                 </div>
-                                <p className="card-footer">
-                                    30 Mins &nbsp; | &nbsp; 1 Serving
-                                </p>
+                                <p></p>
                             </div>
                         </div>
                     </div>
@@ -70,6 +68,12 @@ const StyledWrapper = styled.div`
     transition: transform 300ms;
     box-shadow: 0px 0px 10px 1px #000000ee;
     border-radius: 5px;
+  }
+
+  .content-text {
+    padding: 5px 40px 5px 10px;
+    text-align: justify;
+    font-size: 14px;
   }
 
   .front, .back {
@@ -167,19 +171,25 @@ const StyledWrapper = styled.div`
 
   .title {
     font-size: 11px;
-    max-width: 100%;
+    max-width: 90%;
     display: flex;
-    justify-content: space-between;
+    justify-content: start;
+    gap: 10px;
   }
 
   .title p {
     width: 50%;
   }
 
-  .card-footer {
+  .project-link {
     color: #ffffff88;
-    margin-top: 5px;
-    font-size: 8px;
+    font-size: 14px;
+    cursor: pointer;
+  }
+  
+  .project-link:hover {
+    color: #ffffff;
+    text-decoration: underline;
   }
 
   .front .img {
--- src/views/Project.js
+++ src/views/Project.js
@@ -2,36 +2,51 @@ import Cards from '../components/Cards';
 import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import polling from './../assets/projects/poling_img.png';
+import blog_img from './../assets/projects/blog_img.png';
 
 function Project() {
     const cards = [
         {
-            project_name: "Employee Review System",
+            project_name: "Tech Blog Portal",
             back: "Card 4 Back",
-            image: "https://github.com/Yashaswi-Anand/RealTime-Chat-Frontend/raw/master/screenshots/Online.png",
+            image: blog_img,
             content: ``
         },
         {
-            project_name: "Time2Achieve",
-            back: "Card 1 Back",
-            image: "https://raw.githubusercontent.com/Yashaswi-Anand/Time2Achieve/master/images/new.png"
+            project_name: "Real Time Chat Application",
+            back: "Card 4 Back",
+            image: "https://github.com/Yashaswi-Anand/RealTime-Chat-Frontend/raw/master/screenshots/View.png",
+            content: ``
         },
         {
             project_name: "Polling System API",
             back: "Card 2 Back",
-            image: polling
+            image: polling,
+            content: ''
         },
         {
             project_name: "Habit Tracker",
             back: "Card 3 Back",
-            image: "https://github.com/Yashaswi-Anand/Habit-Tracker/raw/master/screenshots/habits.png"
+            image: "https://github.com/Yashaswi-Anand/Habit-Tracker/raw/master/screenshots/habits.png",
+            content: `This application that allow to track your habits. Any user register and login this application after that views habit details and track their habits.`
         },
         {
             project_name: "Employee Review System",
             back: "Card 4 Back",
             image: "https://github.com/Yashaswi-Anand/EmployeeReviewSystem/raw/master/screenshots/signup.png",
             content: `This application that allow to submit feedback toward each other's performance. Any employee register and login this application after that views performance rating and review their peformance for gives some feedbacks.`
-        }
+        },
+        {
+            project_name: "Project Management System",
+            back: "Card 5 Back",
+            image: "https://github.com/Yashaswi-Anand/Project-Management-System/raw/master/screenshots/login2.png",
+            content: `This application that allow to manage project and task. Any employee register and login this application after that views project and task details.`
+        },
+        {
+            project_name: "Time2Achieve",
+            back: "Card 1 Back",
+            image: "https://raw.githubusercontent.com/Yashaswi-Anand/Time2Achieve/master/images/new.png"
+        },
     ];
     return (
         <div>
@@ -40,7 +55,7 @@ function Project() {
             </div>
             <div className='card-container'>
                 {cards && cards.map((card, index) => (
-                    <Cards data={card} key={index}/>
+                    <Cards data={card} key={index} />
                 ))}
             </div>
 
